d2fced864eee564228dd0b8492909ee13759c734
Isale <isale.bryan@gmail.com>
2021-08-11 11:07:49 +0300

    raphestates

--- profiletest.php
+++ profiletest.php
@@ -11,7 +11,7 @@
     <meta name="author" content="">
     <link rel="shortcut icon" href="../../assets/ico/favicon.ico">
 
-    <title>DreamVilla</title>
+    <title>RaphEstates</title>
 
     <!-- Bootstrap core CSS -->
     <link href="css/bootstrap.min.css" rel="stylesheet">
@@ -40,7 +40,7 @@
       
       <div class="navbar navbar-inverse">
      <div class="container navc">
-     <a class="navbar-brand" href="#"><font size="10" color="white">DreamVilla</font></a>
+     <a class="navbar-brand" href="#"><font size="10" color="white">RaphEstates</font></a>
      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
         <span class="icon-bar"></span>
         <span class="icon-bar"></span>
@@ -485,8 +485,8 @@
           <div class="foot" style="min-height:350px;background:#4D4C4C;margin-top:5%">
         <div class="row">
           <div class="col-md-6">
-            <h1 style="color:White;font-size:35px;text-align:center;margint-top:5%;font-family: 'Lobster', cursive;">DreamVilla</h1>
-            <p style="color:#CCC;font-size:20px;margin-left:5%;">DreamVilla is India's No 1 Property portal and has been Awarded as the most preferred property site in India, by independent surveys. 
+            <h1 style="color:White;font-size:35px;text-align:center;margint-top:5%;font-family: 'Lobster', cursive;">RaphEstates</h1>
+            <p style="color:#CCC;font-size:20px;margin-left:5%;">RaphEstates is India's No 1 Property portal and has been Awarded as the most preferred property site in India, by independent surveys. 
             Our Webiste provides a platform for property buyers and sellers to locate properties of interest and helps the buyer and seller to communicate
             with each other and provide umambiguous services.</p>
           </div>
